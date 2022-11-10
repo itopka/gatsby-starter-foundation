26c445dbad563e71f799b3d7c21e084ebc011b27
itopka <117648815+itopka@users.noreply.github.com>
2022-11-10 13:37:45 +0300

    Update blog-list-home.js

--- src/components/blog-list-home.js
+++ src/components/blog-list-home.js
@@ -29,7 +29,7 @@ const PostMaker = ({ data }) => (
         variant: "variants.button",
       }}
     >
-      See more
+      Побачити більше
       <span className="icon -right">
         <RiArrowRightSLine />
       </span>
